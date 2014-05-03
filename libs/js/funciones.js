3d5b01fc17e2802d1b2be5e8509e2ebf4dfb6dab
webmasterft <webmasterft@gmail.com>
2014-05-02 23:17:27 -0500

    Reloj en lengua

--- libs/js/funciones.js
+++ libs/js/funciones.js
@@ -8,6 +8,12 @@
 
 
 
+
+
+
+
+
+
 /* COMPARAR ARRAYS*/
     $.extend({
         compare: function (arrayA, arrayB) {
@@ -136,113 +142,248 @@ function toLetters(n)
 
 
 
+
 /* RELOJ */
-var Stopwatch = function(elem, options) {
-  
-  var timer       = createTimer(),
-      startButton = createButton("start", start),
-      stopButton  = createButton("stop", stop),
-      resetButton = createButton("reset", reset),
-      offset,
-      clock,
-      interval;
-  
-  // default options
-  options = options || {};
-  options.delay = options.delay || 1;
- 
-  // append elements     
-  elem.appendChild(timer);
-  elem.appendChild(startButton);
-  elem.appendChild(stopButton);
-  elem.appendChild(resetButton);
-  
-  // initialize
-  reset();
-  
-  // private functions
-  function createTimer() {
-    return document.createElement("span");
-  }
-  
-  function createButton(action, handler) {
-    var a = document.createElement("a");
-    a.href = "#" + action;
-    a.innerHTML = action;
-    a.addEventListener("click", function(event) {
-      handler();
-      event.preventDefault();
-    });
-    return a;
-  }
-  
-  function start() {
-    if (!interval) {
-      offset   = Date.now();
-      interval = setInterval(update, options.delay);
-    }
-  }
-  
-  function stop() {
-    if (interval) {
-      clearInterval(interval);
-      interval = null;
-    }
-  }
-  
-  function reset() {
-    clock = 0;
-    render(0);
-  }
-  
-  function update() {
-    clock += delta();
-    render();
-  }
-  
-  function render() {
-    timer.innerHTML = clock/1000; 
-  }
-  
-  function delta() {
-    var now = Date.now(),
-        d   = now - offset;
+/*
+ * =======================
+ * jQuery Timer Plugin
+ * =======================
+ * 
+ * Depends on:    jquery
+ * 
+ * --------
+ * Summary:
+ * --------
+ * Start/Stop/Resume a time in any HTML element
+ */
+
+ (function($){
+
+  var jQueryTimer = function(element, options) {
+    var defaults = {
+      action: 'start',
+      editable: true   //this will let users make changes to the time
+    };
+
+    this.options = $.extend(defaults, options);
+    this.$el = $(element);
+
+
     
-    offset = now;
-    return d;
+
+    this.init();
+    
+  };
+
+  /*
+    Initialize the plugin with common properties
+  */
+  jQueryTimer.prototype.init = function() {
+
+    //setup
+    this.secsNum           = 0;
+    this.minsNum           = 0;
+    this.hrsNum            = 0;
+    this.secsStr           = "0 sec";
+    this.minsStr           = "";
+    this.hrsStr            = "";
+    this.timerId           = null;
+    this.delay             = 1000;
+    this.isTimerRunning    = false;
+
+    if (this.options.seconds !== undefined) {
+      this.hrsNum = Math.floor(this.options.seconds / 3600);
+      this.minsNum = Math.floor((this.options.seconds - (this.hrsNum * 3600))/60);
+      this.secsNum = this.options.seconds - (this.hrsNum * 3600) - (this.minsNum * 60);
+      
+      this.timeToString();
+    }
+    
+    this.elType = this.$el.prop('tagName').toLowerCase();
+
+    if(this.options.editable) this.initEditable();
+
+  };
+
+  jQueryTimer.prototype.start = function () {
+    if(!this.isTimerRunning) {
+      this.updateTimerDisplay();
+      this.incrementTime(); //to avoid the 1 second gap that gets created if the seconds are not incremented
+      this.startTimerInterval();
+    }
+  }
+
+  jQueryTimer.prototype.pause = function () {
+    clearInterval(this.timerId);
+    this.isTimerRunning = false;
+  }
+
+  jQueryTimer.prototype.resume = function () {
+    if(!this.isTimerRunning) this.startTimerInterval();
+  }
+
+  
+  
+  
+  jQueryTimer.prototype.startTimerInterval = function () {
+    var self = this;
+    this.timerId = setInterval(function() { self.incrementTime() }, this.delay);
+    this.isTimerRunning = true;
+  }
+
+  /*
+    Allow users to click and edit the timer value by typing in
+  */
+  jQueryTimer.prototype.initEditable = function () {
+    var self = this;
+    this.$el.on('focus', function(){
+      self.pause();
+    });
+
+    this.$el.on('blur', function(){
+      //get the value and update the number of seconds if necessary
+      var timerDisplayStr;
+
+      //remove any spaces while getting the string
+      if(self.elType == 'input' || self.elType == 'textarea') timerDisplayStr = $(this).val().replace(/\s+/, '');
+      else timerDisplayStr = $(this).html().replace(/\s+/, '');
+
+      //check for seconds
+      //check for minutes
+      //check for hours
+
+      var matchSeconds  = /\d+sec/,
+          matchMinutes  = /\d+\:\d+min/,
+          matchHours    = /\d+\:\d+\:\d+/;
+
+      if(timerDisplayStr.match(matchSeconds)) {
+        //extract the seconds from this
+        self.secsNum = parseInt(timerDisplayStr.replace(/sec/, ''), 10) + 1;
+        if (self.secsNum > 59) {
+          self.secsNum = 00;
+          self.minsNum++;
+        }
+      } else if(timerDisplayStr.match(matchMinutes)) {
+        timerDisplayStr = timerDisplayStr.replace(/min/, '');
+        var timerDisplayArr = timerDisplayStr.split(':');
+        self.minsNum = parseInt(timerDisplayArr[0], 10);
+        self.secsNum = parseInt(timerDisplayArr[1], 10) + 1;
+
+        if (self.secsNum > 59) {
+          self.secsNum = 00;
+          self.minsNum++;
+        }
+
+        if (self.minsNum > 59) {
+          self.minsNum = 00;
+          self.hrsNum++;
+        }
+
+      } else if(timerDisplayStr.match(matchHours)) {
+        var timerDisplayArr = timerDisplayStr.split(':');
+        self.hrsNum = parseInt(timerDisplayArr[0], 10);
+        self.minsNum = parseInt(timerDisplayArr[1], 10);
+        self.secsNum = parseInt(timerDisplayArr[2], 10) + 1;
+
+        if (self.secsNum > 59) {
+          self.secsNum = 00;
+          self.minsNum++;
+        }
+
+        if (self.minsNum > 59) {
+          self.minsNum = 00;
+          self.hrsNum++;
+        }
+      }
+      self.resume();
+    });
   }
   
-  // public API
-  this.start  = start;
-  this.stop   = stop;
-  this.reset  = reset;
-};
+  jQueryTimer.prototype.updateTimerDisplay = function () {
+    //if(this.hrsNum > 0) this.options.showHours = true;
+    /*if(this.options.showHours) this.$el.html(this.hrsStr + ":" + this.minsStr + ":" + this.secsStr);
+    else this.$el.html(this.minsStr + ":" + this.secsStr);*/
+    var displayStr;
+    if(this.hrsNum == 0) {
+      if(this.secsNum < 60 && this.minsNum == 0) displayStr = this.secsStr + ' sec';
+      else displayStr = this.minsStr + ":" + this.secsStr + ' min';
+    } else {
+      displayStr = this.hrsStr + ':' + this.minsStr + ':' + this.secsStr;
+    }
+
+    if(this.elType == 'input' || this.elType == 'textarea') this.$el.val(displayStr);
+    else this.$el.html(displayStr);
+
+    //assign the number of seconds to this element's data attribute for seconds
+    this.$el.data('seconds', this.get_seconds());
+  }
+  
+  jQueryTimer.prototype.timeToString = function () {
+    this.secsStr = ((this.minsNum > 0 || this.hrsNum > 0) && this.secsNum < 10) ?  '0' + this.secsNum : this.secsNum;
+    this.minsStr = (this.hrsNum > 0 && this.minsNum < 10) ?  '0' + this.minsNum : this.minsNum;
+    this.hrsStr = this.hrsNum;
+  }
+
+  /*
+    Get the timer's value in seconds
+  */
+  jQueryTimer.prototype.get_seconds = function () {
+    return ((this.hrsNum*3600) + (this.minsNum*60) + this.secsNum);
+  }
+  
+  jQueryTimer.prototype.incrementTime = function () {
+    this.timeToString();
+    this.updateTimerDisplay();
+    
+    this.secsNum++;
+    if(this.secsNum % 60 == 0) {
+      this.minsNum++;
+      this.secsNum = 0;
+    }
+    
+    //handle time exceeding 60 minsNum!
+    if(this.minsNum > 59 && this.minsNum % 60 == 0)
+    {
+      this.hrsNum++;
+      this.minsNum = 0;
+    }
+  }
+  
 
 
-// basic examples
-var elems = document.getElementsByClassName("basic");
-
-for (var i=0, len=elems.length; i<len; i++) {
-  new Stopwatch(elems[i]);
-}
 
 
-// programmatic examples
-var a = document.getElementById("a-timer");
-aTimer = new Stopwatch(a);
-aTimer.start();
+  ///////////////////////////////////////////////////
+  ///////////////INITIALIZE THE PLUGIN///////////////
+  var pluginName = 'timer';
+  $.fn[pluginName] = function(options) {
 
-var b = document.getElementById("b-timer");
-bTimer = new Stopwatch(b, {delay: 100});
-bTimer.start();
+    return this.each(function() {
 
-var c = document.getElementById("c-timer");
-cTimer = new Stopwatch(c, {delay: 456});
-cTimer.start();
+      // only allow the plugin to be instantiated once
+      if (!( $.data( this, 'plugin_' + pluginName ) instanceof jQueryTimer )) {
+        $.data( this, 'plugin_' + pluginName, new jQueryTimer(this, options) );
+      }
 
-var d = document.getElementById("d-timer");
-dTimer = new Stopwatch(d, {delay: 1000});
-dTimer.start();
+      var instance = $.data( this, 'plugin_' + pluginName );
 
+      if(typeof options == 'string') {
+        if(typeof instance[options] == 'function') {
+          instance[options].call(instance); //passing in 'instance' to provide for the value of 'this' in the called function
+        }
+      }
+    });
+  }
+  ////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////
+
+
+
+ })(jQuery);
 
 /* RELOJ */
+
+
+
+
+
