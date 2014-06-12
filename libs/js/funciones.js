c0286ca3f64c614ad2043add5d0f7de9c47280f3
Fernando Torres <webmasterft@gmail.com>
2014-06-12 14:49:19 -0500

    Cartas 70%

--- libs/js/funciones.js
+++ libs/js/funciones.js
@@ -603,3 +603,17 @@ function handleCardDropBalanza( event, ui ) {
 
 
 
+(function ($) {
+    var oAddClass = $.fn.addClass;
+    $.fn.addClass = function () {
+        for (var i in arguments) {
+            var arg = arguments[i];
+            if ( !! (arg && arg.constructor && arg.call && arg.apply)) {
+                arg();
+                delete arg;
+            }
+        }
+        return oAddClass.apply(this, arguments);
+    }
+
+})(jQuery);
